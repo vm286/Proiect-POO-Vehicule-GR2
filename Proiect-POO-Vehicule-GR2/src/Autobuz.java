4bf18c37eab1b1325608b6a4a238ee324bdc3c78
Claudia Năstăsache <cn250@student.ugal.ro>
2024-11-19 01:42:59 +0200

    proiect

--- Proiect-POO-Vehicule-GR2/src/Autobuz.java
+++ Proiect-POO-Vehicule-GR2/src/Autobuz.java
@@ -1,20 +1,32 @@
 public class Autobuz extends Vehicul {
+    private int capacitatePasageri;
+    private int linie;
 
-       String tipCombustibil;
-       int capacitatePasageri;
+    // Constructor fără argumente
+    public Autobuz() {
+        super();
+        this.capacitatePasageri = 0;
+        this.linie = 0;
+    }
 
-       public Autobuz(int id, String culoare, String marca, int capacitatePasageri, int anFabricatie, String tipCombustibil){
-           super(id, culoare, marca, anFabricatie);
-           this.capacitatePasageri=capacitatePasageri;
-           this.tipCombustibil=tipCombustibil;
-       }
+    // Constructor cu toate argumentele
+    public Autobuz(int id, String marca, int anFabricatie, String culoare, int capacitatePasageri, int linie) {
+        super(id, marca, anFabricatie, culoare);
+        this.capacitatePasageri = capacitatePasageri;
+        this.linie = linie;
+    }
 
-       public Autobuz (){
+    // Constructor de copiere
+    public Autobuz(Autobuz other) {
+        super(other);
+        this.capacitatePasageri = other.capacitatePasageri;
+        this.linie = other.linie;
+    }
 
-       }
-
+    // Metoda toString
     @Override
     public String toString() {
-        return super.toString();
+        return String.format("Autobuz (ID: %d): %s [%d pasageri, linia %d]",
+                super.getId(), super.getMarca(), capacitatePasageri, linie);
     }
 }
--- Proiect-POO-Vehicule-GR2/src/Motocicleta.java
+++ Proiect-POO-Vehicule-GR2/src/Motocicleta.java
@@ -1,20 +1,32 @@
-public class Motocicleta extends Vehicul{
-   String model;
-   int capacitateCilindrica;
+public class Motocicleta extends Vehicul {
+    private int cilindree;
+    private boolean areAtas;
 
-   public Motocicleta(int id, String culoare, String marca, String model, int anFabricatie, int capacitateCilindrica){
-       super(marca, anFabricatie, culoare, id);
-       this.model=model;
-       this.capacitateCilindrica=capacitateCilindrica;
-   }
-
-   public Motocicleta (){
-
-   }
-
-    @Override
-    public String toString() {
-        return super.toString();
+    // Constructor fără argumente
+    public Motocicleta() {
+        super();
+        this.cilindree = 0;
+        this.areAtas = false;
     }
 
+    // Constructor cu toate argumentele
+    public Motocicleta(int id, String marca, int anFabricatie, String culoare, int cilindree, boolean areAtas) {
+        super(id, marca, anFabricatie, culoare);
+        this.cilindree = cilindree;
+        this.areAtas = areAtas;
+    }
+
+    // Constructor de copiere
+    public Motocicleta(Motocicleta other) {
+        super(other);
+        this.cilindree = other.cilindree;
+        this.areAtas = other.areAtas;
+    }
+
+    // Metoda toString
+    @Override
+    public String toString() {
+        return "Motocicleta " + super.getMarca() + " cu " + cilindree + " cm³" +
+                (areAtas ? " și ataș." : " fără ataș.");
+    }
 }
