2a06070779ed5600f5307851ad7cea59f064fbb7
mirea <mirea.cristian2004@gmail.com>
2024-11-19 00:21:14 +0200

    Tren si Vehicul

---
+++ Proiect-POO-Vehicule-GR2/src/Vehicul.java
@@ -0,0 +1,39 @@
+public class Vehicul {
+    private int id;
+    private String marca;
+    private int anFabricatie;
+    private String culoare;
+
+    // Constructor fără argumente
+    public Vehicul() {
+        this.id = 0;
+        this.marca = "Necunoscut";
+        this.anFabricatie = 0;
+        this.culoare = "Necunoscut";
+    }
+
+    // Constructor cu toate argumentele
+    public Vehicul(int id, String marca, int anFabricatie, String culoare) {
+        this.id = id;
+        this.marca = marca;
+        this.anFabricatie = anFabricatie;
+        this.culoare = culoare;
+    }
+
+    // Constructor de copiere
+    public Vehicul(Vehicul other) {
+        this.id = other.id;
+        this.marca = other.marca;
+        this.anFabricatie = other.anFabricatie;
+        this.culoare = other.culoare;
+    }
+
+    // Metoda toString
+    @Override
+    public String toString() {
+        return "Vehicul [ID: " + id + ", Marca: " + marca + ", An: " + anFabricatie + ", Culoare: " + culoare + "]";
+    }
+
+    // Getteri și setteri (opțional)
+}
+
