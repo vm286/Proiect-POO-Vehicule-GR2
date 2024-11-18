d21a119a2cbac82d69cfbfed82a181f7784a98ab
Claudia Năstăsache <cn250@student.ugal.ro>
2024-11-19 00:04:50 +0200

    proiect

--- Proiect-POO-Vehicule-GR2/src/Autobuz.java
+++ Proiect-POO-Vehicule-GR2/src/Autobuz.java
@@ -1,2 +1,17 @@
 public class Autobuz {
+       String marca;
+       String tipCombustibil;
+       int capacitatePasageri;
+       int anFabricatie;
+       public Autobuz(String marca, int capacitatePasageri, int anFabricatie, String tipCombustibil){
+           this.marca=marca;
+           this.capacitatePasageri=capacitatePasageri;
+           this.anFabricatie=anFabricatie;
+           this.tipCombustibil=tipCombustibil;
+       }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- Proiect-POO-Vehicule-GR2/src/Motocicleta.java
+++ Proiect-POO-Vehicule-GR2/src/Motocicleta.java
@@ -1,3 +1,18 @@
 public class Motocicleta {
+   String marca;
+   String model;
+   int anFabricatie;
+   int capacitateCilindrica;
 
+   public Motocicleta(String marca, String model, int anFabricatie, int capacitateCilindrica){
+       this.marca=marca;
+       this.model=model;
+       this.anFabricatie=anFabricatie;
+       this.capacitateCilindrica=capacitateCilindrica;
+   }
+
+    @Override
+    public String toString() {
+
+    }
 }
