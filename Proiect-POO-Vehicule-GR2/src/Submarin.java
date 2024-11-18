2d498bd21269a1f3d1ed9560de111c701e4a0342
vm286 <vm286@student.ugal.ro>
2024-11-18 22:29:49 +0200

    structura

---
+++ Proiect-POO-Vehicule-GR2/src/Submarin.java
@@ -0,0 +1,12 @@
+public class Submarin {
+private String
+    @Override
+    public String toString() {
+        if(Marca=="Not Defined"){
+            return "Nu a fost definit.";
+        }
+        else{
+            return "Avion de marca "+Marca+", de model "+Model+".";
+        }
+    }
+}
