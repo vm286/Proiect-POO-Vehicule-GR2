5b990f824d737648d67f49180cc7454cebfb58e4
mirea <mirea.cristian2004@gmail.com>
2024-11-19 09:26:47 +0200

    Tren si Vehicul

--- Proiect-POO-Vehicule-GR2/src/Main.java
+++ Proiect-POO-Vehicule-GR2/src/Main.java
@@ -8,7 +8,7 @@ System.out.println(av);
 System.out.println(sub);
 Tractor tractor1= new Tractor(22,"Same",2010,"ROSU",110, "functional" );
 System.out.println(tractor1);
-Tren IR1801 = new Tren(1801,"CFR",2004,"ALBASTRU",3,"NORMAL");
-System.out.println(IR1801);
+Tren IR1802 = new Tren(1802,"CFR",2004,"ALBASTRU",3,"NORMAL");
+System.out.println(IR1802);
     }
 }
