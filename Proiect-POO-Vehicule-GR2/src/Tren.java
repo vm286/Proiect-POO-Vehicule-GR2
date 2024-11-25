1850f17cce26947270171ca3f44b066c11f639c4
mirea <mirea.cristian2004@gmail.com>
2024-11-25 23:19:03 +0200

    Da

--- Proiect-POO-Vehicule-GR2/src/Tren.java
+++ Proiect-POO-Vehicule-GR2/src/Tren.java
@@ -1,19 +1,29 @@
 public class Tren extends Vehicul {
     private int numarVagoane;
     private String tipTren;
+    private boolean esteElectric;
+    private String ruta;
+    private int vitezaMaxima;
+
 
     // Constructor fără argumente
     public Tren() {
         super();
         this.numarVagoane = 0;
+        this.esteElectric = false;
+        this.ruta = "Necunoscut";
+        this.vitezaMaxima = 0;
         this.tipTren = "Necunoscut";
     }
 
     // Constructor cu argumentele
-    public Tren(int id, String marca, int anFabricatie, String culoare, int numarVagoane, String tipTren) {
+    public Tren(int id, String marca, int anFabricatie, String culoare, int numarVagoane, String tipTren, boolean electric, String ruta, int vitezaMaxima) {
         super(id, marca, anFabricatie, culoare);
         this.numarVagoane = numarVagoane;
         this.tipTren = tipTren;
+        this.esteElectric = electric;
+        this.ruta = ruta;
+        this.vitezaMaxima = vitezaMaxima;
     }
 
     // Constructor de copiere
@@ -21,11 +31,15 @@ public class Tren extends Vehicul {
         super(other);
         this.numarVagoane = other.numarVagoane;
         this.tipTren = other.tipTren;
+        this.esteElectric = other.esteElectric;
+        this.ruta = other.ruta;
+        this.vitezaMaxima = other.vitezaMaxima;
     }
 
     // Metoda toString
     @Override
     public String toString() {
-        return super.toString() + " este Tren [Număr Vagoane: " + numarVagoane + ", Tip: " + tipTren + "]";
+        return super.toString() + " este Tren [Număr Vagoane: " + numarVagoane + ", Tip: " + tipTren + (esteElectric ? " este electric" : " nu este electrica") + ", Ruta: " + ruta +
+                ", Viteza Maxima: " + vitezaMaxima + " km/h" + "]";
     }
 }
