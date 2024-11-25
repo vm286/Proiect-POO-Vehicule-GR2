187fa7a9ab45753a22ebacca340dd0b7c850eac1
vm286 <vm286@student.ugal.ro>
2024-11-25 22:57:34 +0200

    newsub

--- Proiect-POO-Vehicule-GR2/src/Submarin.java
+++ Proiect-POO-Vehicule-GR2/src/Submarin.java
@@ -1,15 +1,21 @@
 public class Submarin extends Vehicul {
-private String model;
-private int tonaj;
-public Submarin(String model, int tonaj, int id, int anFabricatie, String culoare ) {
+private String model,tipCombustibil,tipArmament;
+private int tonaj,adancimeMax;
+public Submarin(String model, int tonaj, int id, int anFabricatie, String culoare, int adancimeMax, String tipCombustibil, String tipArmament ) {
     super(id,"Submarin", anFabricatie, culoare );
     this.model = model;
     this.tonaj = tonaj;
+    this.adancimeMax = adancimeMax;
+    this.tipCombustibil = tipCombustibil;
+    this.tipArmament = tipArmament;
 }
 public Submarin(Submarin s) {
     super(s);
     this.model = s.model;
     this.tonaj = s.tonaj;
+    this.adancimeMax = s.adancimeMax;
+    this.tipCombustibil = s.tipCombustibil;
+    this.tipArmament = s.tipArmament;
 }
 public Submarin() {
     super();
