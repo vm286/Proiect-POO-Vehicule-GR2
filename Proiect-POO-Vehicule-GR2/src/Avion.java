8a1b551f8b12e9a966eb909b8cadcee40ba302e0
vm286 <vm286@student.ugal.ro>
2024-11-25 22:47:37 +0200

    newavion

--- Proiect-POO-Vehicule-GR2/src/Avion.java
+++ Proiect-POO-Vehicule-GR2/src/Avion.java
@@ -5,13 +5,21 @@ public Avion(){
     super();
     Model = "";
 }
-public Avion(String Marca, String Model, int id, int anFabricatie, String culoare, ){
+public Avion(String Marca, String Model, int id, int anFabricatie, String culoare, int maxrange, int pssnr, int cargolim, String tipMotor) {
     super(id, Marca, anFabricatie, culoare );
     this.Model = Model;
+    this.maxrange = maxrange;
+    this.pssnr = pssnr;
+    this.cargolim = cargolim;
+    this.tipMotor = tipMotor;
 }
 public Avion(Avion a){
     super(a);
     this.Model = a.Model;
+    this.maxrange = a.maxrange;
+    this.pssnr = a.pssnr;
+    this.cargolim = a.cargolim;
+    this.tipMotor = a.tipMotor;
 }
 
 
@@ -21,7 +29,7 @@ public Avion(Avion a){
         return "Nu a fost definit.";
     }
     else{
-     return "Avion de marca "+super.getMarca()+", de model "+Model+", ";
+     return "Avion de marca "+super.getMarca()+", de model "+Model+", care poate transporta "+this.pssnr+" de oameni, "+this.cargolim+" de tone de marfa cu motor tip "+this.tipMotor;
     }
     }
 }
