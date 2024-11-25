93f6c1f4661f2ad95024d17203e9c20e4d340b91
ionas22 <ionradu010@gmail.com>
2024-11-25 23:36:53 +0200

    altceva2

---
+++ Proiect-POO-Vehicule-GR2/src/Test_Radu.java
@@ -0,0 +1,8 @@
+public class Test_Radu {
+    public static void main(String[] args) {
+        Masina masina1 = new Masina(4,"Dacia",2010,"Rosu",1459,"sedan");
+        System.out.println(masina1);
+        Tractor tractor1= new Tractor(22,"Same",2010,"ROSU",110, "nefunctional" , 25.5 ,5008 , true);
+        System.out.println(tractor1);
+    }
+}
--- Proiect-POO-Vehicule-GR2/src/Tractor.java
+++ Proiect-POO-Vehicule-GR2/src/Tractor.java
@@ -2,6 +2,8 @@ public class Tractor extends Vehicul{
     private int cai_putere;
     private String starea_curenta;
     private double consum_combustibil;
+    private int ore_functionare;
+    private boolean incarcator;
 
     //Constructor fara argumente
     public Tractor(){
@@ -9,13 +11,17 @@ public class Tractor extends Vehicul{
         this.cai_putere = 0;
         this.starea_curenta = "nespecificat";
         this.consum_combustibil = 0;
+        this.ore_functionare = 0;
+        this.incarcator = false;
     }
     //Constructor cu toate argumentele
-    public Tractor(int id, String marca, int anFabricatie , String culoare , int cai_putere, String starea_curenta , double consum_combustibil){
+    public Tractor(int id, String marca, int anFabricatie , String culoare , int cai_putere, String starea_curenta , double consum_combustibil , int ore_functionare , boolean incarcator){
         super(id,marca,anFabricatie,culoare);
         this.cai_putere = cai_putere;
         this.starea_curenta = starea_curenta;
         this.consum_combustibil = consum_combustibil;
+        this.ore_functionare = ore_functionare;
+        this.incarcator = incarcator;
     }
     //Constrictor de copiere
     public Tractor(Tractor tr){
@@ -23,11 +29,13 @@ public class Tractor extends Vehicul{
         this.cai_putere = tr.cai_putere;
         this.starea_curenta = tr.starea_curenta;
         this.consum_combustibil = tr.consum_combustibil;
+        this.ore_functionare = tr.ore_functionare;
+        this.incarcator = tr.incarcator;
     }
     //Metoda toString
     @Override
     public String toString(){
-        return "Tractorul " + super.getMarca() + " cu puterea de " + cai_putere + " cp si are un consum de " + consum_combustibil + " si este " + starea_curenta;
+        return "Tractorul " + super.getMarca() + " cu puterea de " + cai_putere + " cp si are un consum de " + consum_combustibil + " si este " + starea_curenta + " si acum are " + ore_functionare + " ore de functionare si " + (incarcator?" are incarcator ":" nu are incarcator ")+ ".";
     }
 
 
