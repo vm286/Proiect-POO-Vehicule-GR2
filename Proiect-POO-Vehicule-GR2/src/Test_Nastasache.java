defbe979a9886754d3c35125917e65b80a645da9
Claudia Năstăsache <cn250@student.ugal.ro>
2024-11-25 23:40:03 +0200

    proiect

--- Proiect-POO-Vehicule-GR2/src/Test_Nastasache.java
+++ Proiect-POO-Vehicule-GR2/src/Test_Nastasache.java
@@ -8,5 +8,15 @@ public class Test_Nastasache {
         for (Motocicleta m : Motocicleta.getListaMotociclete()) {
             System.out.println(m);
         }
+        System.out.println("lista de motociclete electrice cu atas este: ");
+        for (Motocicleta n : Motocicleta.getListaMotociclete()) {
+            if (n.isEsteElectric() && n.isAreAtas())
+                System.out.println(n);
+        }
+        System.out.println("lista de motociclete electrice fara atas este: ");
+        for (Motocicleta n : Motocicleta.getListaMotociclete()) {
+            if (n.isEsteElectric() && !n.isAreAtas())
+                System.out.println(n);
+        }
     }
 }
