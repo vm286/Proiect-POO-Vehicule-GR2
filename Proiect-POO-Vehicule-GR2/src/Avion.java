1fcc24cbb6c88ae9152354343061246ded4a8ef5
vm286 <vm286@student.ugal.ro>
2024-11-25 23:14:11 +0200

    avionlist

--- Proiect-POO-Vehicule-GR2/src/Avion.java
+++ Proiect-POO-Vehicule-GR2/src/Avion.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Avion extends Vehicul {
 private String  Model,tipMotor;
 private int maxrange,pssnr,cargolim;
@@ -21,7 +23,19 @@ public Avion(Avion a){
     this.cargolim = a.cargolim;
     this.tipMotor = a.tipMotor;
 }
+    //vector 10 elemente tip ArrayList
+    public static ArrayList<Avion> getListaAvioane() {
+        ArrayList<Avion> avioane = new ArrayList<>();
+        Avion Mcdonnel = new Avion("Mcdonnel", "DC117", 1, 1967, "Argintiu", 3000, 78, 25, "Turboprop x 4");
+        Avion Boeing = new Avion("Boeing", "777", 2, 2008, "Alb", 23000, 99, 150, "Turbofan x 4");
+        Avion Airbus = new Avion("Airbus", "A220", 3, 2012, "Alb", 22500, 120, 225, "Turbofan x 4");
+        Avion Dassault = new Avion("Dassault", "Mirage", 4, 1978, "Industrial Grey", 3000, 1, 12, "Jet x 1");
+        Avion Fairchild = new Avion("Fairchild", "A-10", 5, 1977, "Gri", 3000, 2, 8, "Jet x 2");
 
+
+
+        return avioane;
+    }
 
     @Override
     public String toString() {
