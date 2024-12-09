a388f9dd4dc52a178c1dbf8933faef57ce18876d
ionas22 <ionradu010@gmail.com>
2024-12-10 00:33:25 +0200

    altceva

--- Proiect-POO-Vehicule-GR2/src/Test_Radu.java
+++ Proiect-POO-Vehicule-GR2/src/Test_Radu.java
@@ -26,29 +26,8 @@ public class Test_Radu {
             System.out.println(m);
         }
 
-        Tractor JohnDeere = new Tractor(24, "John Deere", 2015, "VERDE", 150, "cauciucuri", 30.0, 7000, false);
-        Tractor MasseyFerguson = new Tractor(20, "Massey Ferguson", 2012, "ROSU", 120, "cauciucuri", 28.0, 6000, false);
-        Tractor NewHolland = new Tractor(18, "New Holland", 2018, "ALBASTRU", 140, "cauciucuri", 32.5, 4800, true);
-        Tractor Fendt = new Tractor(26, "Fendt", 2020, "VERDE", 300, "senile", 35.0, 5500, true);
-        Tractor Case = new Tractor(25, "Case", 2017, "GALBEN", 280, "senile", 29.5, 6400, false);
-        Tractor Valtra = new Tractor(20, "Valtra", 2014, "NEGRU", 130, "cauciucuri", 27.0, 5900, true);
-        Tractor DeutzFahr = new Tractor(30, "Deutz-Fahr", 2019, "VERDE", 175, "cauciucuri", 33.5, 5100, false);
-        Tractor Kubota = new Tractor(16, "Kubota", 2016, "PORTOCALIU", 100, "cauciucuri", 26.0, 4500, false);
-        Tractor Zetor = new Tractor(18, "Zetor", 2013, "ROSU", 125, "cauciucuri", 28.5, 5300, false);
-        Tractor Belarus = new Tractor(23, "Belarus", 2021, "ALB", 190, "cauciucuri", 34.0, 5700, true);
 
-        ArrayList<Tractor> tractoare= new ArrayList<>();
-        tractoare.add(JohnDeere);
-        tractoare.add(MasseyFerguson);
-        tractoare.add(NewHolland);
-        tractoare.add(Fendt);
-        tractoare.add(Case);
-        tractoare.add(Valtra);
-        tractoare.add(DeutzFahr);
-        tractoare.add(Kubota);
-        tractoare.add(Zetor);
-        tractoare.add(Belarus);
-
+        ArrayList<Tractor> tractoare = Tractor.getListaTractor();
         //for_each
         System.out.println("Lista de tractoare este: ");
         for (Tractor p : tractoare) {
